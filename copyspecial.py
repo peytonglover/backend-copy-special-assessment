607ee1d907d8a4575d54a11368fa0b96f61b90bc
peyton glover <peyton.vx@protonmail.com>
2020-06-24 13:35:30 -0400

    tests are working

--- copyspecial.py
+++ copyspecial.py
@@ -20,31 +20,35 @@ import argparse
 def get_special_paths(dirname):
     """Given a dirname, returns a list of all its special files."""
     path_list = []
-    pattern = re.compile(r'__(.+?)__')
     for path in os.listdir(dirname):
-        fullpath = os.path.abspath(path)
-        if pattern.search(fullpath):
-            path_list.append(fullpath)
-    print('\n'.join(path_list))
-    return '\n'.join(path_list)
+        pattern = re.search(r'__(\w+)__', path)
+        if pattern:
+            path_list.append(os.path.abspath(os.path.join(dirname, path)))
+    return path_list
+
 
 
 def copy_to(path_list, dest_dir):
+    """Copies files in path_list to new destination"""
     # your code here
-    os.makedirs(dest_dir)
-    for files in path_list:
-        shutil.copy(files, dest_dir)
+    if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
+    for paths in path_list:
+        filename = os.path.basename(paths)
+        shutil.copy(paths, os.path.join(dest_dir, filename))
 
 
 def zip_to(path_list, dest_zip):
+    """Creates a zip file of files in path_list"""
     # your code here
-    abspaths = '\n'.join(path_list)
-    print(
-        """Command im going to do:
-        zip -j """ + dest_zip + ' ' + abspaths
-    )
-    for path in path_list:
-        subprocess.Popen(['zip', '-j', dest_zip, path])
+    cmd = ['zip', '-j', dest_zip]
+    cmd.extend(path_list)
+    try:
+        subprocess.check_output(cmd)
+    except subprocess.CalledProcessError as e:
+        print(e.output)
+        raise
+
 
 
 def main(args):
@@ -53,7 +57,7 @@ def main(args):
     parser = argparse.ArgumentParser()
     parser.add_argument('--todir', help='dest dir for special files')
     parser.add_argument('--tozip', help='dest zipfile for special files')
-    parser.add_argument('--from_dir', help='name of dir to search')
+    parser.add_argument('from_dir', help='name of dir to search')
     # TODO: add one more argument definition to parse the 'from_dir' argument
     ns = parser.parse_args(args)
 
@@ -64,20 +68,26 @@ def main(args):
     # This is input data validation. If something is wrong (or missing) with
     # any required args, the general rule is to print a usage message and
     # exit(1).
-
+    if not ns:
+        parser.print_usage()
+        return
     # Your code here: Invoke (call) your functions
+    if not ns.from_dir:
+        parser.print_usage()
+        return
+
     to_dir = ns.todir
     to_zip = ns.tozip
     from_dir = ns.from_dir
+    path_list = get_special_paths(from_dir)
 
-    if from_dir:
-        path_list = get_special_paths(from_dir)
-    elif to_dir:
-        path_list = get_special_paths(from_dir).split('\n')
+
+    if to_dir:
         copy_to(path_list, to_dir)
     elif to_zip:
-        path_list = get_special_paths(from_dir).split('\n')
         zip_to(path_list, to_zip)
+    else:
+        print('\n'.join(get_special_paths(from_dir)))
 
 
 if __name__ == "__main__":
